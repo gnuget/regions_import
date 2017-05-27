17dace5e540062402e3c924431a04f626a14f8d1
David Valdez <david.valdez210@gmail.com>
2017-05-26 20:03:06 -0500

    Add RegioNTerm source

---
+++ src/Plugin/migrate/source/RegionTerm.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\migrate_example\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
+
+/**
+ * This is an example of a simple SQL-based source plugin. Source plugins are
+ * classes which deliver source data to the processing pipeline. For SQL
+ * sources, the SqlBase class provides most of the functionality needed - for
+ * a specific migration, you are required to implement the three simple public
+ * methods you see below.
+ *
+ * This annotation tells Drupal that the name of the MigrateSource plugin
+ * implemented by this class is "beer_term". This is the name that the migration
+ * configuration references with the source "plugin" key.
+ *
+ * @MigrateSource(
+ *   id = "region_term"
+ * )
+ */
+class RegionTerm extends SourcePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function initializeIterator() {
+    // File handler using our custom header-rows-respecting extension of SPLFileObject.
+    $file = file_get_contents($this->configuration['path']);
+    $json = json_decode($file);
+
+    // Use the array key as the term ID.
+    foreach ($json as $key => $region) {
+      $json[$key]->id = $key;
+    }
+    return $json;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __toString() {
+    return 'RegionTermSource';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    /**
+     * This method simply documents the available source fields provided by
+     * the source plugin, for use by front-end tools. It returns an array keyed
+     * by field/column name, with the value being a translated string explaining
+     * to humans what the field represents. You should always
+     */
+    $fields = [
+      'id' => $this->t('Source ID'),
+      'name' => $this->t('name'),
+      'path' => $this->t('path'),
+    ];
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    /**
+     * This method indicates what field(s) from the source row uniquely identify
+     * that source row, and what their types are. This is critical information
+     * for managing the migration. The keys of the returned array are the field
+     * names from the query which comprise the unique identifier. The values are
+     * arrays indicating the type of the field, used for creating compatible
+     * columns in the map tables that track processed items.
+     */
+    return [
+      'id' => [
+        'type' => 'string',
+      ],
+    ];
+  }
+
+}
+
+
+$cosa = new RegionTerm();
+print_r($cosa->initializeIterator());
